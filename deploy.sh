a2e7c93a46a504e2109ad53d330d1943b20e05ee
asmsuechan <suenagaryoutaabc@gmail.com>
2018-12-10 21:41:44 +0900

    Change to use region option on deploy

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ sed -i -e "s/aws-secret-access-key/${AWS_SECRET_ACCESS_KEY_ID}/" config.json
 sed -i -e "s/sshot-assets/${BUCKET_NAME}/" config.json
 sed -i -e "s/region/${REGION}/" config.json
 
-serverless deploy
+serverless deploy --stage production --region $REGION
